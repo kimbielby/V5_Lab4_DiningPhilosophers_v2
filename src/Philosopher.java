11e0ad73785ae06ebb7c1c91bc9a0911d0c0246d
kimbielby <kib00075@students.stir.ac.uk>
2021-10-20 18:53:34 +0100

    Added classes PhiloNorm and PhiloNice.  Declared Semaphores in StartPhilosophising class as well as boolean.

--- src/Philosopher.java
+++ src/Philosopher.java
@@ -1,4 +1,11 @@
 import java.util.concurrent.Semaphore;
 
-public class Philosopher {
+public class Philosopher extends Thread {
+
+    protected Semaphore chopL;
+    protected Semaphore chopR;
+    protected boolean isNice;
+
+
+
 }
--- src/StartPhilosophising.java
+++ src/StartPhilosophising.java
@@ -1,4 +1,10 @@
 import java.util.concurrent.Semaphore;
 
 public class StartPhilosophising {
+
+    private static Semaphore chop1;
+    private static Semaphore chop2;
+    private static Semaphore chop3;
+
+
 }
